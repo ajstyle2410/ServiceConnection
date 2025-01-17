e5f2b82c412b8a09348f3683351fcd14c973d069
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2025-01-17 23:39:48 +0530

    upload project

---
+++ ServiceConnection/src/main/java/com/service/connection/ServiceConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.service.connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConnectionApplication.class, args);
+	}
+
+}
---
+++ ServiceConnection/src/test/java/com/service/connection/ServiceConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
